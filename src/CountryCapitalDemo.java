b26589fb39adcbe6fae92c46cdcb67156b8f6660
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-19 11:43:23 +0400

    Task 15: Store and Manage Country Capitals in a Java HashMap

---
+++ src/CountryCapitalDemo.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class CountryCapitalDemo {
+    public static void main(String[] args) {
+        HashMap<String, String> CountryCapital = new HashMap<>();
+        CountryCapital.put("Saudi Arabia", "Riyadh");
+        CountryCapital.put("Egypt", "Cairo");
+        CountryCapital.put("Japan", "Tokyo");
+        CountryCapital.put("France", "Paris");
+        CountryCapital.put("Brazil", "Brasilia");
+
+        System.out.println(" all  Country  :" + CountryCapital );
+
+        System.out.println(" capital of one specific country :" +  CountryCapital.get("Egypt, Cairo"));
+
+        boolean nameexitis = CountryCapital.containsKey("JapanTokyo");
+
+        System.out.println("Is in the see Country : " + nameexitis);
+
+        System.out.println("result clearly Country :" +CountryCapital);
+
+        CountryCapital.put("Italy", "Rome");
+
+        System.out.println("updated Country : " + CountryCapital);
+
+        CountryCapital.remove("France", "Paris") ;
+
+        CountryCapital.remove("Japan", "Tokyo");
+
+        System.out.println("after removal Country : " + CountryCapital);
+
+        System.out.println("total number Country :" + CountryCapital.size());
+
+
+
+
+
+    }
+}
